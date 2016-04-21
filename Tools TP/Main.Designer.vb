418851979aa2e4c665df535cda476145ca6d49cb
Omisakin Oluwatobi Samuel <omisakin.oluwatobi@gmail.com>
2016-04-21 22:22:46 +0300

    initial commit for the wifi Ninja

---
+++ Tools TP/Main.Designer.vb
@@ -0,0 +1,384 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(main))
+        Me.netNameBox = New System.Windows.Forms.TextBox()
+        Me.netname = New System.Windows.Forms.Label()
+        Me.creatNetBut = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.passwordbox = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.stopNet = New System.Windows.Forms.RadioButton()
+        Me.startNet = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.goNetStart = New System.Windows.Forms.Button()
+        Me.helpstrip = New System.Windows.Forms.MenuStrip()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HowTo = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutTo = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ToolNotify = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.notifyContext = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.showContext = New System.Windows.Forms.ToolStripMenuItem()
+        Me.startStopContext = New System.Windows.Forms.ToolStripMenuItem()
+        Me.startStopMenu = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.startContext = New System.Windows.Forms.ToolStripMenuItem()
+        Me.stopContext = New System.Windows.Forms.ToolStripMenuItem()
+        Me.aboutContext = New System.Windows.Forms.ToolStripMenuItem()
+        Me.helpContext = New System.Windows.Forms.ToolStripMenuItem()
+        Me.exitContext = New System.Windows.Forms.ToolStripMenuItem()
+        Me.defaultCheckBox = New System.Windows.Forms.CheckBox()
+        Me.warnTxt = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtResults = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.helpstrip.SuspendLayout()
+        Me.notifyContext.SuspendLayout()
+        Me.startStopMenu.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'netNameBox
+        '
+        Me.netNameBox.Location = New System.Drawing.Point(111, 37)
+        Me.netNameBox.Name = "netNameBox"
+        Me.netNameBox.Size = New System.Drawing.Size(132, 20)
+        Me.netNameBox.TabIndex = 0
+        '
+        'netname
+        '
+        Me.netname.AutoSize = True
+        Me.netname.Location = New System.Drawing.Point(44, 44)
+        Me.netname.Name = "netname"
+        Me.netname.Size = New System.Drawing.Size(43, 13)
+        Me.netname.TabIndex = 2
+        Me.netname.Text = "Name:"
+        '
+        'creatNetBut
+        '
+        Me.creatNetBut.Location = New System.Drawing.Point(156, 137)
+        Me.creatNetBut.Name = "creatNetBut"
+        Me.creatNetBut.Size = New System.Drawing.Size(87, 23)
+        Me.creatNetBut.TabIndex = 4
+        Me.creatNetBut.Text = "Create"
+        Me.creatNetBut.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(23, 81)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(253, 12)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "(Only letters, undersore and numbers allowed without space)"
+        '
+        'passwordbox
+        '
+        Me.passwordbox.Location = New System.Drawing.Point(111, 102)
+        Me.passwordbox.Name = "passwordbox"
+        Me.passwordbox.Size = New System.Drawing.Size(132, 20)
+        Me.passwordbox.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(22, 109)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(65, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Password:"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(35, 217)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(47, 23)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "Close"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'stopNet
+        '
+        Me.stopNet.AutoSize = True
+        Me.stopNet.Location = New System.Drawing.Point(57, 8)
+        Me.stopNet.Name = "stopNet"
+        Me.stopNet.Size = New System.Drawing.Size(51, 17)
+        Me.stopNet.TabIndex = 11
+        Me.stopNet.TabStop = True
+        Me.stopNet.Text = "Stop"
+        Me.stopNet.UseVisualStyleBackColor = True
+        '
+        'startNet
+        '
+        Me.startNet.AutoSize = True
+        Me.startNet.Location = New System.Drawing.Point(6, 8)
+        Me.startNet.Name = "startNet"
+        Me.startNet.Size = New System.Drawing.Size(52, 17)
+        Me.startNet.TabIndex = 12
+        Me.startNet.TabStop = True
+        Me.startNet.Text = "Start"
+        Me.startNet.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.stopNet)
+        Me.GroupBox1.Controls.Add(Me.startNet)
+        Me.GroupBox1.Location = New System.Drawing.Point(103, 209)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(127, 31)
+        Me.GroupBox1.TabIndex = 13
+        Me.GroupBox1.TabStop = False
+        '
+        'goNetStart
+        '
+        Me.goNetStart.Location = New System.Drawing.Point(237, 217)
+        Me.goNetStart.Name = "goNetStart"
+        Me.goNetStart.Size = New System.Drawing.Size(39, 23)
+        Me.goNetStart.TabIndex = 14
+        Me.goNetStart.Text = "GO!"
+        Me.goNetStart.UseVisualStyleBackColor = True
+        '
+        'helpstrip
+        '
+        Me.helpstrip.BackColor = System.Drawing.Color.Gray
+        Me.helpstrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.HelpToolStripMenuItem, Me.AboutTo})
+        Me.helpstrip.Location = New System.Drawing.Point(0, 0)
+        Me.helpstrip.Name = "helpstrip"
+        Me.helpstrip.Size = New System.Drawing.Size(295, 24)
+        Me.helpstrip.TabIndex = 15
+        Me.helpstrip.Text = "helpstrip"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.HowTo})
+        Me.HelpToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'HowTo
+        '
+        Me.HowTo.Name = "HowTo"
+        Me.HowTo.Size = New System.Drawing.Size(121, 22)
+        Me.HowTo.Text = "How To?"
+        '
+        'AboutTo
+        '
+        Me.AboutTo.Name = "AboutTo"
+        Me.AboutTo.Size = New System.Drawing.Size(52, 20)
+        Me.AboutTo.Text = "About"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(111, 122)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(164, 12)
+        Me.Label4.TabIndex = 16
+        Me.Label4.Text = "(Password Must be up to 8 characters)"
+        '
+        'ToolNotify
+        '
+        Me.ToolNotify.ContextMenuStrip = Me.notifyContext
+        Me.ToolNotify.Icon = CType(resources.GetObject("ToolNotify.Icon"), System.Drawing.Icon)
+        Me.ToolNotify.Text = "ToolNotifyIcon"
+        Me.ToolNotify.Visible = True
+        '
+        'notifyContext
+        '
+        Me.notifyContext.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.showContext, Me.startStopContext, Me.aboutContext, Me.helpContext, Me.exitContext})
+        Me.notifyContext.Name = "notifyContext"
+        Me.notifyContext.Size = New System.Drawing.Size(128, 114)
+        '
+        'showContext
+        '
+        Me.showContext.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.showContext.Name = "showContext"
+        Me.showContext.Size = New System.Drawing.Size(127, 22)
+        Me.showContext.Text = "Show"
+        Me.showContext.ToolTipText = "Unhide the Hotspot Creator"
+        '
+        'startStopContext
+        '
+        Me.startStopContext.DropDown = Me.startStopMenu
+        Me.startStopContext.Name = "startStopContext"
+        Me.startStopContext.Size = New System.Drawing.Size(127, 22)
+        Me.startStopContext.Text = "Start/Stop"
+        '
+        'startStopMenu
+        '
+        Me.startStopMenu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.startContext, Me.stopContext})
+        Me.startStopMenu.Name = "startStopMenu"
+        Me.startStopMenu.OwnerItem = Me.startStopContext
+        Me.startStopMenu.Size = New System.Drawing.Size(170, 48)
+        '
+        'startContext
+        '
+        Me.startContext.Name = "startContext"
+        Me.startContext.Size = New System.Drawing.Size(169, 22)
+        Me.startContext.Text = "Start Wifi HotSpot"
+        '
+        'stopContext
+        '
+        Me.stopContext.Name = "stopContext"
+        Me.stopContext.Size = New System.Drawing.Size(169, 22)
+        Me.stopContext.Text = "Stop Wifi Hotspot"
+        '
+        'aboutContext
+        '
+        Me.aboutContext.Name = "aboutContext"
+        Me.aboutContext.Size = New System.Drawing.Size(127, 22)
+        Me.aboutContext.Text = "About"
+        '
+        'helpContext
+        '
+        Me.helpContext.Name = "helpContext"
+        Me.helpContext.Size = New System.Drawing.Size(127, 22)
+        Me.helpContext.Text = "Help"
+        '
+        'exitContext
+        '
+        Me.exitContext.Name = "exitContext"
+        Me.exitContext.Size = New System.Drawing.Size(127, 22)
+        Me.exitContext.Text = "Exit"
+        '
+        'defaultCheckBox
+        '
+        Me.defaultCheckBox.AutoSize = True
+        Me.defaultCheckBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.defaultCheckBox.Location = New System.Drawing.Point(113, 61)
+        Me.defaultCheckBox.Name = "defaultCheckBox"
+        Me.defaultCheckBox.Size = New System.Drawing.Size(127, 17)
+        Me.defaultCheckBox.TabIndex = 18
+        Me.defaultCheckBox.Text = "Use Default Name"
+        Me.defaultCheckBox.UseVisualStyleBackColor = True
+        '
+        'warnTxt
+        '
+        Me.warnTxt.AutoSize = True
+        Me.warnTxt.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.warnTxt.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.warnTxt.Location = New System.Drawing.Point(38, 21)
+        Me.warnTxt.Name = "warnTxt"
+        Me.warnTxt.Size = New System.Drawing.Size(0, 13)
+        Me.warnTxt.TabIndex = 19
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(32, 175)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 13)
+        Me.Label2.TabIndex = 20
+        Me.Label2.Text = "Hotspot:"
+        '
+        'txtResults
+        '
+        Me.txtResults.Location = New System.Drawing.Point(113, 175)
+        Me.txtResults.Multiline = True
+        Me.txtResults.Name = "txtResults"
+        Me.txtResults.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtResults.Size = New System.Drawing.Size(130, 36)
+        Me.txtResults.TabIndex = 21
+        '
+        'main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(295, 252)
+        Me.Controls.Add(Me.txtResults)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.warnTxt)
+        Me.Controls.Add(Me.defaultCheckBox)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.goNetStart)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.passwordbox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.creatNetBut)
+        Me.Controls.Add(Me.netname)
+        Me.Controls.Add(Me.netNameBox)
+        Me.Controls.Add(Me.helpstrip)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.HelpButton = True
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.helpstrip
+        Me.MaximizeBox = False
+        Me.Name = "main"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Hotspot Maker"
+        Me.TopMost = True
+        Me.TransparencyKey = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.helpstrip.ResumeLayout(False)
+        Me.helpstrip.PerformLayout()
+        Me.notifyContext.ResumeLayout(False)
+        Me.startStopMenu.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    ' Private WithEvents AxWebBrowser1 As AxXtremeSuiteControls.AxWebBrowser
+    'Private WithEvents AxResizer1 As AxXtremeSuiteControls.AxResizer
+    Friend WithEvents netNameBox As System.Windows.Forms.TextBox
+    Friend WithEvents netname As System.Windows.Forms.Label
+    Friend WithEvents creatNetBut As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents passwordbox As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents stopNet As System.Windows.Forms.RadioButton
+    Friend WithEvents startNet As System.Windows.Forms.RadioButton
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents goNetStart As System.Windows.Forms.Button
+    Friend WithEvents helpstrip As System.Windows.Forms.MenuStrip
+    Friend WithEvents HelpToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HowTo As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents AboutTo As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolNotify As System.Windows.Forms.NotifyIcon
+    Friend WithEvents notifyContext As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents showContext As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents startStopContext As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents startStopMenu As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents startContext As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents stopContext As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents aboutContext As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents helpContext As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents exitContext As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents defaultCheckBox As System.Windows.Forms.CheckBox
+    Friend WithEvents warnTxt As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtResults As System.Windows.Forms.TextBox
+
+End Class
---
+++ Tools TP/Main.vb
@@ -0,0 +1,227 @@
+﻿Public Class main
+    Dim stoptext = "netsh wlan stop hostednetwork"
+    Dim starttext = "netsh wlan start hostednetwork"
+    Dim checkstattxt = "netsh wlan show hostednetwork"
+    Dim thewholetext = ""
+    Private activeornot = False
+    Private Function thestopstart() As String
+        Return thewholetext
+    End Function
+    Public Sub getter(ByVal command As String)
+        thewholetext = command
+    End Sub
+
+    Private Function setter() As String
+        Return thewholetext
+
+    End Function
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        defaultCheckBox.Enabled = False
+        'Ax1.Controls.Add(New System.Windows.Forms.Button)
+        ' Dim abc As New XtremeSuiteControls.DateTimePicker
+        'Me.AddOwnedForm(abc)
+        ' Dim a = AxWebBrowser2.LocationURL
+        'AxWebBrowser2.Navigate("google.com")
+        'netsh wlan show hostednetwor
+
+        getter(checkstattxt)
+        Dim CMDThread2 As New Threading.Thread(AddressOf CMDAutomate)
+        CMDThread2.Start()
+        CMDThread2.Abort()
+
+
+        If activeornot = True Then
+            startContext.Enabled = False
+            stopContext.Enabled = True
+        Else
+            startContext.Enabled = True
+            stopContext.Enabled = False
+        End If
+
+    End Sub
+
+    Private Results As String
+    Private Delegate Sub delUpdate()
+    Private Finished As New delUpdate(AddressOf UpdateText)
+    Public textResult As String = ""
+
+    Private Sub UpdateText()
+        textResult = Results
+        txtResults.Text = textResult
+    End Sub
+
+    Private Sub errorChecker()
+        If textResult.Contains("administrator privilege") Then
+            warnTxt.Text = "Run Wifi Ninja as Administrator"
+        Else
+            warnTxt.Text = ""
+        End If
+    End Sub
+    Friend netexistornot As Boolean
+
+
+
+    Public Function getNetVal() As String
+        Dim thenametext As String
+        Dim theprevtext As String
+        Dim thepasstext As String
+        Dim thefullcomm As String
+        thenametext = netNameBox.Text
+        thepasstext = passwordbox.Text
+        theprevtext = "netsh wlan set hostednetwork"
+        thefullcomm = theprevtext & " mode=allow ssid=" & thenametext & " key =" & thepasstext
+        Return thefullcomm
+    End Function
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles creatNetBut.Click
+        If (netNameBox.Text = "" Or netNameBox.Text.Contains(" ") Or passwordbox.Text = "" Or passwordbox.Text.Contains(" ") Or netNameBox.Text.Length < 3 Or passwordbox.Text.Length < 8) Then
+            MsgBox("Invalid Input", MsgBoxStyle.Exclamation)
+            passwordbox.Clear()
+        Else
+            getter(getNetVal())
+            setter()
+            Dim CMDThread As New Threading.Thread(AddressOf CMDAutomate)
+            CMDThread.Start()
+            startNet.Checked = False
+            stopNet.Checked = False
+            If textResult.Contains("The SSID of the hosted network has been successfully changed.") Then
+                warnTxt.Text = textResult
+                MsgBox("Successfully Created", MsgBoxStyle.OkOnly)
+            Else
+                warnTxt.Text = textResult
+                MsgBox("Not Successfully Created", MsgBoxStyle.Exclamation)
+            End If
+            errorChecker()
+        End If
+
+    End Sub
+   
+    Private Sub CMDAutomate()
+        Dim myprocess As New Process
+        Dim StartInfo As New System.Diagnostics.ProcessStartInfo
+        StartInfo.FileName = "cmd" 'starts cmd window
+        StartInfo.RedirectStandardInput = True
+        StartInfo.RedirectStandardOutput = True
+        StartInfo.UseShellExecute = False 'required to redirect
+        StartInfo.CreateNoWindow = True 'creates no cmd window
+        myprocess.StartInfo = StartInfo
+        myprocess.Start()
+        Dim SR As System.IO.StreamReader = myprocess.StandardOutput
+        Dim SW As System.IO.StreamWriter = myprocess.StandardInput
+        SW.WriteLine(thewholetext) 'the command you wish to run.....
+        SW.WriteLine("exit") 'exits command prompt window
+        Results = SR.ReadToEnd 'returns results of the command window
+        SW.Close()
+        SR.Close()
+        'invokes Finished delegate, which updates textbox with the results text
+        Invoke(Finished)
+    End Sub
+
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Me.Hide()
+        ToolNotify.Visible = True
+        ToolNotify.BalloonTipText = "The Wifi Hotspot Creator is still running here! Right-click Icon to view Menu"
+        ToolNotify.BalloonTipTitle = "Wifi Ninja"
+        ToolNotify.Text = "Wifi Nija - Hospot Creator"
+        ToolNotify.ShowBalloonTip(6000)
+    End Sub
+
+    Private Sub startNet_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles startNet.CheckedChanged
+    End Sub
+    Private Sub starterstopper()
+        If startNet.Checked = True Then
+            thewholetext = starttext
+            Dim CMDThread2 As New Threading.Thread(AddressOf CMDAutomate)
+            CMDThread2.Start()
+            errorChecker()
+            If textResult.Contains("The hosted network started.") Then
+                activeornot = True 'active or not, active = true, notactive = false
+                MsgBox("Successfully Started", MsgBoxStyle.OkOnly)
+            Else
+                MsgBox("Not Successfully Started", MsgBoxStyle.OkOnly)
+            End If
+        ElseIf stopNet.Checked = True Then
+            thewholetext = stoptext
+            Dim CMDThread2 As New Threading.Thread(AddressOf CMDAutomate)
+            CMDThread2.Start()
+            errorChecker()
+            If textResult.Contains("The hosted network started.") Then
+                activeornot = False 'active or not, active = true, notactive = false
+                MsgBox("Successfully Stopped", MsgBoxStyle.OkOnly)
+            Else
+                MsgBox("Not Successfully Stopped", MsgBoxStyle.OkOnly)
+            End If
+        Else
+            MsgBox("Please check all fields!", MsgBoxStyle.Exclamation)
+        End If
+    End Sub
+    Private Sub GroupBox1_Enter(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles GroupBox1.Enter
+    End Sub
+
+    Private Sub stopNet_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles stopNet.CheckedChanged
+    End Sub
+
+    Private Sub Button1_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles goNetStart.Click
+        starterstopper()
+    End Sub
+    Friend Sub exitHandler()
+        If activeornot = True Then
+            Dim i = MsgBox("This will stop your WIFI shared!", MsgBoxStyle.OkCancel)
+            If i = MsgBoxResult.Cancel Then
+
+            ElseIf i = MsgBoxResult.Ok Then
+                thewholetext = stoptext
+                Dim CMDThread2 As New Threading.Thread(AddressOf CMDAutomate)
+                CMDThread2.Start()
+                errorChecker()
+                MsgBox("Net Successfully stopped", MsgBoxStyle.OkOnly)
+                Me.Close()
+            End If
+        Else
+            MsgBox("Closing....If app not closed, you can open again and stop network then exit", MsgBoxStyle.OkOnly)
+            Me.Close()
+        End If
+    End Sub
+    Private Sub main_FormClosing(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
+
+    End Sub
+
+    Private Sub HowTo_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles HowTo.Click
+        MsgBox("Enter a network name without space (only letters, and _ is allowed ), Enter the at least 8 character long password or key (Numbers preferable),  Click Create, Select Start (or Stop), Click OK. **Make sure you 'Set used network network to shared. How To Share? check http://windows.microsoft.com/en-us/windows/using-internet-connection-sharing' ", MsgBoxStyle.Information)
+    End Sub
+    Private Sub AboutTo_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles AboutTo.Click
+        AboutBox1.ShowDialog()
+    End Sub
+
+    Private Sub showContext_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles showContext.Click
+        Me.Show()
+    End Sub
+
+    Private Sub aboutContext_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles aboutContext.Click
+        AboutBox1.ShowDialog()
+    End Sub
+
+    Private Sub helpContext_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles helpContext.Click
+        MsgBox("Enter a network name without space (only letters, and _ is allowed ), Enter the at least 8 character long password or key (Numbers preferable),  Click Create, Select Start (or Stop), Click OK. **Make sure you 'Set used network network to shared. How To Share? check http://windows.microsoft.com/en-us/windows/using-internet-connection-sharing' ", MsgBoxStyle.Information)
+    End Sub
+
+    Private Sub exitContext_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles exitContext.Click
+        exitHandler()
+    End Sub
+
+    Private Sub defaultCheckBox_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles defaultCheckBox.CheckedChanged
+        If defaultCheckBox.Checked Then
+            Dim k1 = textResult.IndexOf("""")
+            Dim k2 = textResult.IndexOf("""", k1 + 1)
+            Dim thessid = textResult.Substring(k1 + (1), (k2 - k1 - 1))
+            netNameBox.Text = thessid
+            netNameBox.Enabled = False
+        End If
+    End Sub
+
+    Private Sub ToolNotify_MouseDoubleClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles ToolNotify.MouseDoubleClick
+    End Sub
+
+End Class
